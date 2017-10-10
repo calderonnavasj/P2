4087677fbc8f4cd5c69fb631c6e7aa3238c0077d
jcalderonnavas <32562262+jcalderonnavas@users.noreply.github.com>
2017-10-10 16:50:22 -0600

    Elka

--- app/src/main/java/com/example/josue/p2/Fotografia.java
+++ app/src/main/java/com/example/josue/p2/Fotografia.java
@@ -1,15 +1,34 @@
 package com.example.josue.p2;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatImageView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Fotografia extends AppCompatActivity {
+
+    private static final int IMAGE_CAPTURE = 102;
+    private static final int VIDEO_CAPTURE = 101;
+    private static final int SELECT_PICTURE = 100;
+    private static final String TAG = "MainActivity";
+
+    CoordinatorLayout coordinatorLayout;
+    FloatingActionButton btnSelectImage;
+    AppCompatImageView imgView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,18 +36,88 @@ public class Fotografia extends AppCompatActivity {
         setContentView(R.layout.activity_fotografia);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        final int REQUEST_IMAGE_CAPTURE = 1;
-
+//-----------------------Tomar fotografía-------------------------------------
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-                }
+                String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+                String FileName = "PIC_" + timeStamp + "_";
+
+                File mediaFile =
+                        new File(Environment.getExternalStorageDirectory().getAbsolutePath()
+                                + "/"+FileName+"mifoto.jpg");
+
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+                Uri videoUri = Uri.fromFile(mediaFile);
+
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, videoUri);
+
+                startActivityForResult(intent, IMAGE_CAPTURE);
             }
         });
+//-----------------------Tomar vídeo-------------------------------------
+        FloatingActionButton video = (FloatingActionButton) findViewById(R.id.fab2);
+        video.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+                String FileName = "MP4_" + timeStamp + "_";
+
+                File mediaFile =
+                        new File(Environment.getExternalStorageDirectory().getAbsolutePath()
+                                + "/"+FileName+"mivideo.mp4");
+
+                Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+
+                Uri videoUri = Uri.fromFile(mediaFile);
+
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, videoUri);
+                startActivityForResult(intent, VIDEO_CAPTURE);
+            }
+        });
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == VIDEO_CAPTURE) {
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(this, "Video saved to:\n" +
+                        data.getData(), Toast.LENGTH_LONG).show();
+            } else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(this, "Video recording cancelled.",
+                        Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Failed to record video",Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+    protected void onActivityResult2(int requestCode, int resultCode, Intent data) {
+        if (requestCode == IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            ImageView mImageView = new ImageView(this);
+            mImageView.setImageBitmap(imageBitmap);
+        }
+    }
+
+    String mCurrentPhotoPath;
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
     }
 
 }
